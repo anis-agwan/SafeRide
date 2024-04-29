cc6b34e7451353f21e20957386ff9f36068e2954
Mo <haroon@Mos-MacBook-Air.lan>
2024-04-28 22:51:27 -0400

    Safe ride app splash screen start

---
+++ lib/main.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  runApp(MyApp(
+    child: MaterialApp(
+      title: 'Safe Ride Demo',
+      theme: ThemeData(
+       primarySwatch: Colors.blue
+      ),
+      home: Scaffold(appBar: AppBar(title: const Text("Safe Ride App"),),)
+    )
+  ));
+}
+
+class MyApp extends StatefulWidget {
+  final Widget? child;
+
+  MyApp({this.child});
+
+  static void restartApp(BuildContext context) {
+    context.findAncestorStateOfType<_MyAppState>()!.restartApp();
+  }
+
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+
+  Key key = UniqueKey();
+  void restartApp() {
+    setState(() {
+      key  = UniqueKey();
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return KeyedSubtree(key: key, child: widget.child!);
+  }
+}
